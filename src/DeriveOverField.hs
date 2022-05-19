9ac9525dbdeb5073982398df38b919ae5cb35edf
Joseph Sumabat <joseph@mercury.com>
2022-05-19 15:52:24 -0400

    Add derive over all fields

--- src/DeriveOverField.hs
+++ src/DeriveOverField.hs
@@ -1,9 +1,20 @@
-module DeriveOverField (deriveOverField) where
+{-# LANGUAGE NamedFieldPuns #-}
+module DeriveOverField (
+  deriveOverField
+ , deriveOverFieldWithOptions
+ , deriveOverAllFields
+) where
 
 import Language.Haskell.TH
+import Data.Set as Set hiding (filter)
+import Data.Maybe
+import Control.Monad
+import qualified Data.Char as Char
 
 -- | Derive an over function for a nested field
+-- overSomeField functions are useful as a composable setter that does not have as much overhead as lenses
 -- Given:
+--
 -- @
 -- data MyType =
 --   ParentType {
@@ -11,7 +22,7 @@ import Language.Haskell.TH
 --   , someOtherField :: Int
 --   }
 --   deriving (Show)
--- 
+--
 -- data MyType2=
 --   MyType2
 --     {
@@ -21,23 +32,68 @@ import Language.Haskell.TH
 --     deriving (Show)
 -- @
 --
+-- then:
+--
+-- @
+
 -- $(deriveOverField 'someField)
+-- @
 --
 -- will give:
 --
--- overSometypeFieldsomeField :: (ChildType -> Childtype) -> Parenttype -> Parenttype 
--- overSometypeFieldsomeField modifyFieldFn parentType = parentType {someField = modifyFieldFn (someField parentType)}
+-- @
+-- overSomefield :: (ChildType -> Childtype) -> Parenttype -> Parenttype
+-- overSomefield modifyFieldFn parentType = parentType {someField = modifyFieldFn (someField parentType)}
+-- @
 deriveOverField :: Name -> Q [Dec]
-deriveOverField fieldName = do
+deriveOverField = deriveOverFieldWithOptions OverField
+
+-- | deriveOverField with configurable function names 
+deriveOverFieldWithOptions :: DeriveOverFnNameOption -> Name  -> Q [Dec]
+deriveOverFieldWithOptions fnNameOption fieldName= do
   let myType = mkName "parentType"
       modifyFieldFn = mkName "modifyFieldFn"
-  fieldType  <- reifyType fieldName
+  fieldType <- reifyType fieldName
   case fieldType of
-    AppT (AppT ArrowT parentType@(ConT parentTypeName)) childType ->
-      let 
-          overType = mkName ("over" <> nameBase parentTypeName <> "Field" <> nameBase fieldName) 
-          typeSig = SigD overType (AppT (AppT ArrowT (AppT (AppT ArrowT childType) childType)) (AppT (AppT ArrowT parentType) parentType))
+    AppT (AppT ArrowT parentType) childType ->
+      let moduleNameAndBaseName n = fromMaybe "" (nameModule n) <> "Module" <> (upperFirst . nameBase) n
+          overName = 
+            case fnNameOption of
+              OverField -> mkName $ "over" <> (upperFirst . normalizeName . nameBase) fieldName
+              OverModuleField -> mkName . normalizeName $ "over" <> moduleNameAndBaseName fieldName
+          -- AST for the type signature: (ChildType -> Childtype) -> Parenttype -> Parenttype
+          typeSig = SigD overName (AppT (AppT ArrowT (AppT (AppT ArrowT childType) childType)) (AppT (AppT ArrowT parentType) parentType))
+          -- AST for the fn definition: overSometypeSomeField modifyFieldFn parentType = parentType {someField = modifyFieldFn (someField parentType)}
           functionDefinition =
-            FunD overType [Clause [VarP modifyFieldFn,VarP myType] (NormalB (RecUpdE (VarE myType) [(fieldName,AppE (VarE modifyFieldFn) (ParensE (AppE (VarE fieldName) (VarE myType))))])) []] in
-      pure [typeSig, functionDefinition]
-    _ -> error "Invalid type input to deriveOverField: input must be a field function"
+            FunD overName [Clause [VarP modifyFieldFn, VarP myType] (NormalB (RecUpdE (VarE myType) [(fieldName, AppE (VarE modifyFieldFn) (ParensE (AppE (VarE fieldName) (VarE myType))))])) []]
+       in do
+         pure [typeSig, functionDefinition]
+    _ -> error "Only simple constructors supported"
+  where
+    upperFirst :: String -> String
+    upperFirst [] = []
+    upperFirst (c : cs) = Char.toUpper c : cs
+    normalizeName :: String -> String
+    normalizeName = filter Char.isAlphaNum
+
+-- | Derive all fields for a type
+-- (Will only derive top level fields)
+-- Note:
+--  - Currently not implemented for newtype
+deriveOverAllFields :: DeriveOverFnNameOption -> Name -> Q [Dec]
+deriveOverAllFields fnNameOption typeName = do
+  info <- reify typeName
+  case info of
+    TyConI (DataD _ _ _ _ [RecC _ rawVarbangs] _) ->
+      let fieldNames = (getVarBangName <$> rawVarbangs)
+       in join <$> mapM (deriveOverFieldWithOptions fnNameOption) fieldNames
+    _ -> reportError "Invalid type input to deriveOverAllFields: input must be a data type constructor." >> pure []
+  where
+    getVarBangName (n, _, _) = n
+
+-- | Set the naming convention for generated functions useful if there are conflicts
+data DeriveOverFnNameOption
+  = -- | function name will be overField if field is a field of Mytype
+    OverField
+  | -- | function name will be overMymoduleField if field is a field of the type Mytype
+    OverModuleField
